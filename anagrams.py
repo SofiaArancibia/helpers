f5eca79e9d1f4ac573a08453a4803e6d4eeed1fe
SofiaArancibia <sofi.aran_05@hotmail.com>
2021-05-28 16:32:45 -0300

    Create anagrams.py

---
+++ anagrams.py
@@ -0,0 +1,6 @@
+def anagrams(s1, s2):
+    s1=s1.lower()
+    s2=s2.lower()
+    l1 = sorted([c for c in s1]) # O(n.logn) + O(n)
+    l2 = sorted([c for c in s2]) # O(n.logn) + O(n)
+    return l1 == l2

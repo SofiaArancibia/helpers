f2b13b4aaee4c2d12712277109165241b76f7e5d
SofiaArancibia <sofi.aran_05@hotmail.com>
2021-05-28 16:33:32 -0300

    Update anagrams.py

--- anagrams.py
+++ anagrams.py
@@ -3,4 +3,6 @@ def anagrams(s1, s2):
     s2=s2.lower()
     l1 = sorted([c for c in s1]) # O(n.logn) + O(n)
     l2 = sorted([c for c in s2]) # O(n.logn) + O(n)
+    # 2*(O(n.logn) + O(n))
     return l1 == l2
+
